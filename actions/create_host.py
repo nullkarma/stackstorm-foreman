e12a1557b98f96aa0d524fc8b837a28f99d263f6
Daniel Chamot <daniel@nullkarma.com>
2017-04-24 22:35:23 +0200

    fixed an enum in provision_method & renamed parameter

--- actions/create_host.py
+++ actions/create_host.py
@@ -8,16 +8,16 @@ class NewHost(ForemanAPI):
     def __init__(self, config):
         super(NewHost, self).__init__(config=config)
 
-    def run(self, unattended, provision_method, name, hosts):
+    def run(self, unattended, provision_method, name, hostconfig):
 
-        hosts['provision_method'] = provision_method
-        payload = {"host": hosts}
+        hostconfig['provision_method'] = provision_method
+        payload = {"host": hostconfig}
 
         if unattended:
             sec, ms = str(time()).split('.')
             suffix = str(int(sec) + int(ms))
-            hosts['name'] = name + suffix
+            hostconfig['name'] = name + suffix
         else:
-            hosts['name'] = name
+            hostconfig['name'] = name
 
         return self.post(endpoint='hosts', verify_ssl=self.verify_ssl, payload=payload)
